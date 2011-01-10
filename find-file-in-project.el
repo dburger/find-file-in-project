c4bf0f17716a2586ae8f9bd7e5831720b8eaaec0
David J. Burger <loosedog@gmail.com>
2011-01-09 21:18:26 -0800

    Adds project cacheing.
    Keeps a hash map of project roots to files in the project.  This
    allows it to avoid re-parsing of a TAGS file or re-running GNU find on
    subsequent invocations.  The cache can be cleared
    with #ffip-clear-project-cache.

--- find-file-in-project.el
+++ find-file-in-project.el
@@ -33,23 +33,40 @@
 
 ;; This library depends on GNU find.
 
-;; This file provides a couple methods for quickly finding any file in
-;; a given project. Projects are defined in two ways. The first uses
-;; `locate-dominating-file'. First, if the `locate-dominating-file'
-;; function is bound, it assumes you are using Emacs 23, in which case
-;; you it will look for a `.dir-locals.el' file in an ancestor
-;; directory of the current file. Otherwise it uses the
+;; This file provides a few methods for quickly finding any file in
+;; a given project. Projects are defined in one of four ways.  For
+;; the first, third, and fourth described methods, GNU find is used
+;; to locate the files within the project.  The second described
+;; method uses TAGS files to define the files for a project.  The
+;; different methods of defining a project are presented here in the
+;; order they will be attempted when `find-file-in-project' is
+;; invoked.
+
+;; The first uses the value of the variable `ffip-project-root' when
+;; this value is non-nil.
+
+;; The second method of defining a project is to use TAGS files if
+;; `ffip-try-tags' is non-nil. It will look for an ancestor directory
+;; of the current file containing a TAGS file.  If found it will
+;; parse the TAGS file for project file names and will not use GNU
+;; find to determine project file names.
+
+;; The third uses `locate-dominating-file'. First, if the
+;; `locate-dominating-file' function is bound, it assumes you are using
+;; Emacs 23, in which case it will look for a `.dir-locals.el' file in
+;; an ancestor directory of the current file. Otherwise it uses the
 ;;`project-local-variables' library, which looks for a `.emacs-project'
 ;; file.
 
-;; The other method takes advantage of the prominence of version
+;; The fourth method takes advantage of the prominence of version
 ;; control systems in projects to quickly identify the tree for a
 ;; project. It does so using `project.el' when available. `project.el'
 ;; is shipped in this tree, but for reasons of encouraging using
 ;; default Emacs behavior when and where possible, you will need to
 ;; manually require it in your Emacs configuration to make use of it.
 
-;; By default, it looks only for files whose names match
+;; The first, third, and fourth methods use GNU find to find the files
+;; withn a project. By default, it looks only for files whose names match
 ;; `ffip-patterns', but it's understood that that variable will be
 ;; overridden locally. This can be done either with a mode hook:
 
@@ -101,33 +118,48 @@ Use this to exclude portions of your project: \"-not -regex \\\".*vendor.*\\\"\"
 (defvar ffip-try-tags nil
   "If non-nil, ffip will first look for a TAGS file to determine project files.")
 
+(defvar ffip-project-cache (make-hash-table :test 'equal)
+  "Cache of project root to files in that project.
+
+Used to remove the need to re-parse the TAGS files or re-run the find
+command on subsequent invocations of ffip-project-files. To clear invoke
+`ffip-clear-project-cache'")
+
+(defun ffip-clear-project-cache ()
+  "Clears the project cache."
+  (interactive)
+  (clrhash ffip-project-cache))
+
 (defun ffip-project-files ()
   "Returns a list of all files in the project.
 
-First looks for a TAGS file and if found uses it to determine
-the project files, otherwise, GNU find is used."
-  (let ((tags-dir
-         (and ffip-try-tags
-              (ffip-locate-dominating-file default-directory "TAGS"))))
-    (if tags-dir
-        (ffip-project-files-from-tags tags-dir)
-      (ffip-project-files-from-find))))
+First determines the project root and checks `ffip-project-cache' for the
+list of files under that key.  If already cached, the cached value is
+returned.  Otherwise, the TAGS file or GNU find is used to determine the
+list of files for the project, which is then cached and returned."
+  (let ((project-root
+         (or (ffip-project-root)
+             (error "No project root found"))))
+    (or (gethash project-root ffip-project-cache)
+        (puthash project-root
+                 (if (file-exists-p (concat project-root "TAGS"))
+                     (ffip-project-files-from-tags project-root)
+                   (ffip-project-files-from-find project-root))
+                 ffip-project-cache))))
 
-(defun ffip-project-files-from-tags (tags-dir)
-  "Returns a list of all files in the TAGS file in TAGS-DIR"
+(defun ffip-project-files-from-tags (project-root)
+  "Returns a list of all files in the TAGS file in PROJECT-ROOT"
   (save-excursion
-    (visit-tags-table tags-dir)
+    (visit-tags-table project-root)
     (visit-tags-table-buffer)
     (tags-table-files)))
 
-(defun ffip-project-files-from-find ()
+(defun ffip-project-files-from-find (project-root)
   "Returns a list of all files in the project using GNU find."
   (interactive)
   (split-string (shell-command-to-string
                  (format "find %s -type f %s %s"
-                         (or ffip-project-root
-                             (ffip-project-root)
-                             (error "no project root found"))
+                         project-root
                          (ffip-join-patterns)
                          ffip-find-options))))
 
@@ -178,13 +210,17 @@ setting the `ffip-project-root' variable."
 (defun ffip-project-root ()
   "Return the root of the project.
 
-If `locate-dominating-file' is bound, it will use Emacs' built-in
-functionality; otherwise it will fall back on the definition from
-project-local-variables.el."
+Returns the first non-nil value among the variable `ffip-project-root',
+the first ancestor directory containing a TAGS file if `ffip-try-tags'
+is non-nil, and the value from `project-root' or
+`ffip-locate-dominating-file'."
   (let ((project-root
-         (if (featurep 'project) (project-root)
-           ;; TODO: provide a list of files that can be fallen back upon
-           (ffip-locate-dominating-file default-directory ffip-project-file))))
+         (or ffip-project-root
+             (and ffip-try-tags
+                  (ffip-locate-dominating-file default-directory "TAGS"))
+             (if (featurep 'project) (project-root)
+               ;; TODO: provide a list of files that can be fallen back upon
+               (ffip-locate-dominating-file default-directory ffip-project-file)))))
     (or project-root
         (progn (message "No project was defined for the current file.")
                nil))))
